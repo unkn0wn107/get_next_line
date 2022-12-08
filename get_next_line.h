ecfb6210b9ff57fc8afb0aa222457cb3606fcac7
ukn0wn107 <unkn0wn@proton.me>
2022-12-08 16:54:14 +0100

    Default BUFFER_SIZE value

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,21 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 02:35:03 by agaley            #+#    #+#             */
-/*   Updated: 2022/12/02 02:49:59 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/12/08 16:53:52 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
-#define BUFFER_SIZE 1024
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
