198c9a6ddb2a1620c8bb253a6fde7b0a48eb7924
ukn0wn107 <unkn0wn@proton.me>
2023-01-05 19:43:22 +0100

    Merge 2 wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 02:36:15 by agaley            #+#    #+#             */
-/*   Updated: 2023/01/04 23:43:06 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2023/01/05 19:41:36 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,4 +87,26 @@ char	*get_next_line(int fd)
 			return (ft_lastlinefrombuff(buff[fd], line));
 	}
 	return (line);
-}
+
+// // Check si -D BUFFER_SIZE=-1 ne plante pas la déclaration
+// char	*get_next_line(int fd)
+// {
+// 	static char		buff[BUFFER_SIZE];
+// 	static ssize_t	cur;
+// 	char			*line;
+// 	ssize_t			read_b;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > SSIZE_MAX)
+// 		return (NULL);
+// 	while (ft_buffchr_nextpos('\n', buff, cur) == -1)
+// 	{
+// 		read_b = read(fd, buff, BUFFER_SIZE);
+// 		if (read_b < 0 || (read_b == 0 && sizeof(line) == 0))
+// 			return (NULL);
+// 		ft_append_line(line, buff, 0, BUFFER_SIZE);
+// 	}
+// 	if (ft_buffchr_nextpos('\n', buff, cur) > 0)
+// 	{
+
+// 	}
+// }
