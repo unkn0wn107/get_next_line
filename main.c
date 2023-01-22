c8ad1db47aa37af28fb69abe1b791fad40f9f3d3
agaley <agaley@student.42lyon.fr>
2023-01-23 00:22:25 +0100

    Fix main

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
 	char	*line;
 
-	int fd = open("1lnonl", O_RDONLY);
+	int fd = open("one_line_no_nl.txt", O_RDONLY);
 	printf("%d\n", fd);
 	line = get_next_line(fd);
 	while (line)
 	{
-		printf("|%s|%lu ", line, strlen(line));
+		printf("%lu|%s", strlen(line), line);
 		free(line);
 		line = get_next_line(fd);
 	}
--- main_bonus.c
+++ main_bonus.c
@@ -8,18 +8,14 @@ int main(void)
 	char	*line;
 	char	*line2;
 
-	int fd = open("one_line_no_nl.txt", O_RDONLY);
-	int fd2 = open("get_next_line.c", O_RDONLY);
+	int fd = open("get_next_line.c", O_RDONLY);
+	int fd2 = open("one_line_no_nl.txt", O_RDONLY);
 
 	printf("%d\n", fd);
 	printf("%d\n", fd2);
 
 	line = get_next_line(fd);
-	printf("%lu|%s", strlen(line), line);
-	free(line);
 	line2 = get_next_line(fd2);
-	printf("%lu|%s", strlen(line2), line2);
-	free(line2);
 
 	while (line)
 	{
